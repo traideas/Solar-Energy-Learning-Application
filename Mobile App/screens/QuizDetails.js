9ba90ea49bbc1cc34f7b0b09b5c84f5ba5700bc8
istiak227 <istiak.ahmed227@gmail.com>
2021-05-27 16:39:53 +0600

    QUIZE WORKS 50%

--- Mobile App/screens/QuizDetails.js
+++ Mobile App/screens/QuizDetails.js
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { StyleSheet, TouchableOpacity } from "react-native";
+import { Alert, StyleSheet, TouchableOpacity } from "react-native";
 import { Block, Button, Text } from "galio-framework";
 import { FlatList } from "react-native-gesture-handler";
 
@@ -40,14 +40,19 @@ export default QuizDetails = ({ navigation, route }) => {
   const { title, description, questions } = route.params;
   const [quizQuestions] = useState(questions);
   const [startQuiz, setStartQuiz] = useState(false);
-  const [currentQuestion, setCurrentQuestion] = useState(2);
-  const [selectedOption, setSelectedOption] = useState();
+  const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [selectedOption, setSelectedOption] = useState(null);
   const startQuizHandler = () => {
     setStartQuiz(true);
   };
   const handlePress = (item) => {
     console.log(item, "Click");
   };
+  const onSubmitAnswer = () => {
+    if (selectedOption === quizQuestions[currentQuestion].answer) {
+      Alert.alert("Your Answer is correct")
+    } else Alert.alert("Wrong Answer")
+  }
   return (
     <Block style={styles.container}>
       <Text>Take Quize: {title}</Text>
@@ -59,7 +64,19 @@ export default QuizDetails = ({ navigation, route }) => {
               <Text h3>{quizQuestions[currentQuestion].question}</Text>
             </Block>
             <Block style={styles.optionsContainer}>
-              <FlatList
+              <TouchableOpacity  onPress={() => setSelectedOption(1)}>
+                <Text h5 style={selectedOption === 1 ? {color: "#0000FF"} : {color: "#000000"}}>Option 1: {quizQuestions[currentQuestion].options_1}</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => setSelectedOption(2)}>
+                <Text h5 style={selectedOption === 2 ? {color: "#0000FF"} : {color: "#000000"}}>Option 2: {quizQuestions[currentQuestion].options_2}</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => setSelectedOption(3)}>
+                <Text h5 style={selectedOption === 3 ? {color: "#0000FF"} : {color: "#000000"}}>Option 3: {quizQuestions[currentQuestion].options_3}</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => setSelectedOption(4)}>
+                <Text h5 style={selectedOption === 4 ? {color: "#0000FF"} : {color: "#000000"}}>Option 4: {quizQuestions[currentQuestion].options_4}</Text>
+              </TouchableOpacity>
+              {/* <FlatList
                 data={quizQuestions[currentQuestion].answerOptions}
                 keyExtractor={(item, index) => "key" + index}
                 renderItem={({ item }) =>
@@ -67,8 +84,9 @@ export default QuizDetails = ({ navigation, route }) => {
                     <Text h5>Option 2: {item.answerText}</Text>
                   </TouchableOpacity>
                 }
-              />
+              /> */}
             </Block>
+            <Button onPress={onSubmitAnswer}>Submit Answer</Button>
             {/* <TakeQuize
               quiz={quizQuestions[currentQuestion]}
               onPress={() => handlePress()}
@@ -93,7 +111,7 @@ const styles = StyleSheet.create({
   },
   question: {},
   optionsContainer: {
-    flex: 1,
+    flex: 0.8,
     justifyContent: "space-evenly",
   },
 });
