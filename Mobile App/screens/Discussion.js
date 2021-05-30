8e2cf832f0dee4ce3109c2889ffc0da47ea0ea48
istiak227 <istiak.ahmed227@gmail.com>
2021-05-30 18:24:13 +0600

    Comment option is working, adding comments on Discussion Topic

--- Mobile App/screens/Discussion.js
+++ Mobile App/screens/Discussion.js
@@ -1,22 +1,49 @@
 import React, { useState } from "react";
 import { StyleSheet } from "react-native";
-import { Block, Input, Text, Button } from "galio-framework";
+import { Block, Text, Button } from "galio-framework";
+import axios from "axios";
+import { useForm } from "react-hook-form";
+
+import Input from '../components/Input'
 
 export default Discussion = ({ route }) => {
-  const { title, description, comments } = route.params;
+  const { title, description, comments, id } = route.params;
   // console.log(comments)
-  const [AllComments] = useState(comments);
-  const renderComments = AllComments.map((comment) => (
-    <Text style={{margin: 10}}>{comment.comment}</Text>
+  const [allComments, setAllComments] = useState(comments);
+  const { control, handleSubmit } = useForm();
+
+  const renderComments = allComments.map((comment, index) => (
+    <Text key={index} style={{ margin: 10 }}>
+      {comment.comment}
+    </Text>
   ));
+
+  const onSubmit = ({ comment }) => {
+    axios
+      .post("http://127.0.0.1:8000/comment/", {
+        comment: comment,
+        discussion: id,
+        created_by: 10,
+      })
+      .then((res) => {
+        console.log("Commented");
+        setAllComments(prevState => [...prevState, {comment: comment}])
+      })
+      .catch((err) => console.log(err));
+  };
 
   return (
     <Block style={styles.container}>
       <Text h4>{title}</Text>
       <Text>{description}</Text>
       <Block style={styles.commentContainer}>{renderComments}</Block>
-      <Input/>
-      <Button>Comment</Button>
+      <Input
+        borderless
+        placeholder="Comment"
+        name="comment"
+        control={control}
+      />
+      <Button onPress={handleSubmit(onSubmit)}>Comment</Button>
     </Block>
   );
 };
@@ -25,7 +52,5 @@ const styles = StyleSheet.create({
   container: {
     padding: 40,
   },
-  commentContainer: {
-    
-  },
+  commentContainer: {},
 });
