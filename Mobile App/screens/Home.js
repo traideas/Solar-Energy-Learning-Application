e82f8f8ff96f41de0fbce516ddc855ed3a52b2bc
limonsafayet <limon6531@gmail.com>
2021-06-02 18:52:45 +0600

    home mobile

--- Mobile App/screens/Home.js
+++ Mobile App/screens/Home.js
@@ -1,8 +1,12 @@
 import React from "react";
-import { StyleSheet, Dimensions, ScrollView, TouchableOpacity } from "react-native";
-import { Block, Text, Card } from "galio-framework";
+import { StyleSheet, Dimensions, ScrollView, TouchableOpacity, ImageBackground } from "react-native";
+
+import { Block, Text, theme, Card } from "galio-framework";
+
+import Images from "../constants/Images";
 
 const { width } = Dimensions.get("screen");
+
 
 export default Home = ({ navigation }) => {
   return (
@@ -10,20 +14,69 @@ export default Home = ({ navigation }) => {
       <Block flex style={styles.home}>
 
         <TouchableOpacity onPress={() => navigation.navigate("Video")}>
-          <Card middle style={styles.card} title="Watch Videos" />
+          <Block flex card style={[styles.category, styles.shadow]}>
+            <ImageBackground
+              source={Images.Pro}
+              style={[
+                styles.imageBlock,
+                { width: width - theme.SIZES.BASE * 2, height: 180 }
+              ]}
+              imageStyle={{
+                width: width - theme.SIZES.BASE * 2,
+                height: 252
+              }}
+            >
+              <Block style={styles.categoryTitle}>
+                <Text style={{ fontFamily: 'open-sans-bold' }} size={18} color={theme.COLORS.WHITE}>
+                  Videos
+                </Text>
+              </Block>
+            </ImageBackground>
+          </Block>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => navigation.navigate("Article")}>
-          <Card style={styles.card} title="Read Articles" />
+          <Block flex card style={[styles.category, styles.shadow]}>
+            <ImageBackground
+              source={Images.Pro}
+              style={[
+                styles.imageBlock,
+                { width: width - theme.SIZES.BASE * 2, height: 180 }
+              ]}
+              imageStyle={{
+                width: width - theme.SIZES.BASE * 2,
+                height: 252
+              }}
+            >
+              <Block style={styles.categoryTitle}>
+                <Text style={{ fontFamily: 'open-sans-bold' }} size={18} color={theme.COLORS.WHITE}>
+                  Articles
+                </Text>
+              </Block>
+            </ImageBackground>
+          </Block>
         </TouchableOpacity>
         <TouchableOpacity onPress={() => navigation.navigate("Slide")}>
-          <Card title="View Slides" />
+          <Block flex card style={[styles.category, styles.shadow]}>
+            <ImageBackground
+              source={Images.Pro}
+              style={[
+                styles.imageBlock,
+                { width: width - theme.SIZES.BASE * 2, height: 180 }
+              ]}
+              imageStyle={{
+                width: width - theme.SIZES.BASE * 2,
+                height: 252
+              }}
+            >
+              <Block style={styles.categoryTitle}>
+                <Text style={{ fontFamily: 'open-sans-bold' }} size={18} color={theme.COLORS.WHITE}>
+                  Slides
+                </Text>
+              </Block>
+            </ImageBackground>
+          </Block>
         </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate("Quiz")}>
-          <Card title="Quizes" />
-        </TouchableOpacity>
-        <TouchableOpacity onPress={() => navigation.navigate("Discussion")}>
-          <Card title="Discussions" />
-        </TouchableOpacity>
+
       </Block>
     </ScrollView>
   );
@@ -40,4 +93,35 @@ const styles = StyleSheet.create({
     borderRadius: 4,
 
   },
+  categories: {
+    width
+  },
+  categoryList: {
+    justifyContent: "center",
+    paddingTop: theme.SIZES.BASE * 1.5
+  },
+  category: {
+    backgroundColor: theme.COLORS.WHITE,
+    marginHorizontal: theme.SIZES.BASE,
+    marginVertical: theme.SIZES.BASE / 2,
+    borderWidth: 0
+  },
+  categoryTitle: {
+    height: "100%",
+    paddingHorizontal: theme.SIZES.BASE,
+    backgroundColor: "rgba(0, 0, 0, 0.5)",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  imageBlock: {
+    overflow: "hidden",
+    borderRadius: 4
+  },
+  shadow: {
+    shadowColor: theme.COLORS.BLACK,
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 3,
+    shadowOpacity: 0.1,
+    elevation: 2
+  }
 });
