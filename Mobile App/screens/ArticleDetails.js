9fff016c3566bb5d015cb2c2105bef3449835c16
istiak227 <istiak.ahmed227@gmail.com>
2021-06-14 21:37:42 +0600

    Pdf viewer added both on Article and slides, need to change slide cause slide has pptx type. But Article is fine. because article only shows pdf. Only need to add a loader

--- Mobile App/screens/ArticleDetails.js
+++ Mobile App/screens/ArticleDetails.js
@@ -1,74 +1,34 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Dimensions, ScrollView } from "react-native";
+import { StyleSheet, Dimensions, ScrollView, View } from "react-native";
 import { Block, Text, Button } from "galio-framework";
 import { WebView } from "react-native-webview";
+import PDFReader from "rn-pdf-reader-js"
 // import { Constants, FileSystem } from "expo";
 // import * as FileSystem from "expo-file-system";
 
 export default ArticleDetails = ({ route }) => {
-  const [isLoading, setLoading] = useState(false);
   const { title, description, file } = route.params;
-  const [pdfUrl, setPdfUrl] = useState("");
-  const [url, seturl] = useState();
-  const { width } = Dimensions.get("window");
 
-
- /*  useEffect(() => {
-    FileSystem.downloadAsync(
-      "http://www.africau.edu/images/default/sample.pdf",
-      FileSystem.documentDirectory + "small.pdf"
-    )
-      .then(({ uri }) => {
-        seturl(uri);
-        console.log("Finished downloading to ", uri);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []); */
- /*  const downloadhandler = () => {
-    FileSystem.downloadAsync(
-      "http://www.africau.edu/images/default/sample.pdf",
-      FileSystem.documentDirectory + "small.pdf"
-    )
-      .then(({ uri }) => {
-        console.log("Finished downloading to ", uri);
-        seturl(uri);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }; */
   return (
-    <ScrollView>
-      <Block style={styles.container}>
-        <Block>
-          <Text h3>{title}</Text>
-          <Text p>Your file will be downloaded shortly</Text>
-        </Block>
-        <WebView
-          originWhitelist={['*']}
-          source={{ uri: file }}
-          javaScriptEnabled={true}
-          domStorageEnabled={true}
-          useWebKit={true}
-          startInLoadingState={true}
-        />
-      </Block>
-    </ScrollView>
+    <View style={styles.container}>
+      <PDFReader
+        style={{
+          width: Dimensions.get("window").width,
+          height: Dimensions.get("window").height
+        }}
+        source={{
+          uri: file,
+        }}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: "flex-start",
+    backgroundColor: "#fff",
     alignItems: "center",
-    marginTop: 25,
-  },
-  pdf: {
-    flex: 1,
-    width: Dimensions.get("window").width,
-    height: Dimensions.get("window").height,
+    justifyContent: "center",
   },
 });
--- Mobile App/screens/SlideDetails.js
+++ Mobile App/screens/SlideDetails.js
@@ -1,35 +1,33 @@
 import React, { useEffect, useState } from "react";
-import { StyleSheet, Dimensions, ScrollView } from "react-native";
+import { StyleSheet, Dimensions, ScrollView, View } from "react-native";
 import { Block, Text } from "galio-framework";
 import { WebView } from "react-native-webview";
-//import PDFReader from "rn-pdf-reader-js"
-
+import PDFReader from "rn-pdf-reader-js"
 
 export default VideoDetail = ({ route }) => {
-  const { title, description, file, photo, upload_date, created_by } = route.params;
+  const { title, description, file, photo, upload_date, created_by } =
+    route.params;
   const { width } = Dimensions.get("window");
   return (
-    <ScrollView>
-      <Block style={styles.container}>
-        <Block>
-          <Text h3>{title}</Text>
-          <Text p>Your file will be downloaded shortly</Text>
-        </Block>
-        <WebView
-          originWhitelist={['*']}
-          source={{ uri: file }}
-          javaScriptEnabled={true}
-          domStorageEnabled={true}
-          useWebKit={true}
-          startInLoadingState={true}
-        />
-      </Block>
-    </ScrollView>
+    <View style={styles.container}>
+      <PDFReader
+        style={{
+          width: Dimensions.get("window").width,
+          height: Dimensions.get("window").height
+        }}
+        source={{
+          uri: file,
+        }}
+      />
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 20,
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
